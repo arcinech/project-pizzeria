2b793e2844e1912caf3dc25db9a014884ba65473
Marcin Lech <arcinech@gmail.com>
2022-03-05 21:05:46 +0100

    Add: sendOrder with getData

--- src/js/script.js
+++ src/js/script.js
@@ -368,6 +368,9 @@
       thisCart.dom.subtotalPrice = thisCart.dom.wrapper.querySelector(select.cart.subtotalPrice);
       thisCart.dom.totalPrice = thisCart.dom.wrapper.querySelectorAll(select.cart.totalPrice);
       thisCart.dom.totalNumber = thisCart.dom.wrapper.querySelector(select.cart.totalNumber);
+      thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
+      thisCart.dom.address =  thisCart.dom.wrapper.querySelector(select.cart.address);
+      thisCart.dom.phone = thisCart.dom.wrapper.querySelector(select.cart.phone);
     }
 
     initActions(){
@@ -391,6 +394,12 @@
       thisCart.dom.productList.addEventListener('edit', function(event){
         event.preventDefault();
       });
+
+      thisCart.dom.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisCart.sendOrder();
+      });
+
     }
     
     add(menuProduct){
@@ -404,6 +413,7 @@
 
       thisCart.products.push(new CartProduct(menuProduct, generatedDOM));
       thisCart.update();
+      console.log(this.products);
     }
 
     update(){
@@ -411,25 +421,25 @@
 
       const deliveryFee = settings.cart.defaultDeliveryFee;
       
-      let totalNumber = 0;
-      let subtotalPrice = 0;
+      thisCart.totalNumber = 0;
+      thisCart.subtotalPrice = 0;
 
       for(let product of thisCart.products){
-        totalNumber += product.amount;
-        subtotalPrice += product.price;
+        thisCart.totalNumber += product.amount;
+        thisCart.subtotalPrice += product.price;
       }
 
-      if (totalNumber > 0) {
-        thisCart.totalPrice = subtotalPrice + deliveryFee;
+      if (thisCart.totalNumber > 0) {
+        thisCart.totalPrice = thisCart.subtotalPrice + deliveryFee;
         thisCart.dom.deliveryFee.innerHTML = deliveryFee;
       } else {
         // Total price and delivery fee after changing cart product to qty 0
-        thisCart.totalPrice = subtotalPrice;
+        thisCart.totalPrice = thisCart.subtotalPrice;
         thisCart.dom.deliveryFee.innerHTML = 0;
       }
 
-      thisCart.dom.totalNumber.innerHTML = totalNumber;
-      thisCart.dom.subtotalPrice.innerHTML = subtotalPrice;
+      thisCart.dom.totalNumber.innerHTML = thisCart.totalNumber;
+      thisCart.dom.subtotalPrice.innerHTML = thisCart.subtotalPrice;
       for (let total of thisCart.dom.totalPrice){
         total.innerHTML = thisCart.totalPrice;
       }
@@ -441,6 +451,27 @@
 
       thisCart.products.splice(indexOfProduct, 1);
       thisCart.dom.productList.removeChild(thisCart.dom.productList.children[indexOfProduct]);
+    }
+
+    sendOrder(){
+      const thisCart = this;
+
+      const url = settings.db.url + '/' + settings.db.orders;
+      const payload = {};
+      payload.products = [];
+      for(let prod of thisCart.products) {
+        payload.products.push(prod.getData());
+      }
+      payload.address =  thisCart.dom.address.value;
+      payload.phone = thisCart.dom.phone.value;
+      payload.totalPrice = thisCart.totalPrice;
+      payload.subtotalPrice = thisCart.subtotalPrice;
+      payload.totalNumber = thisCart.totalNumber;
+      payload.deliverFee = thisCart.dom.deliveryFee.innerHTML;
+
+      console.log('payload', payload);
+
+      return payload;
     }
   }
 
@@ -508,6 +539,18 @@
         event.preventDefault();
         thisCartProduct.remove();
       });
+    }
+
+    getData(){
+      const productData = {
+        id : this.id,
+        amount: this.amount,
+        price: this.price,
+        priceSingle: this.priceSingle,
+        name: this.name,
+        params: this.params,
+      };
+      return productData;
     }
   }
 
