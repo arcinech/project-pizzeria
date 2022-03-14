4ac908b9af3a4366c1e19e8cd379af8253cbf91d
Marcin Lech <arcinech@gmail.com>
2022-03-14 21:01:34 +0100

    Add: initPages

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,63 @@
 
-import { select, settings } from './settings.js';
+import { select, settings, classNames } from './settings.js';
 import Cart from './components/Cart.js';
 import Product from './components/Product.js';
 import Booking from './components/Booking.js';
 
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#', '');
+    console.log(idFromHash);
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(const page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        thisApp.activatePage(id);
+
+        window.location.hash = '#' + id;
+      });
+    }
+
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    for(let page of thisApp.pages){
+      page.classList.toggle(
+        classNames.pages.active, 
+        page.id == pageId
+      );
+    }
+
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.pages.active, 
+        link.getAttribute('href')  == '#' + pageId
+      );
+    }
+  },
 
   initMenu: function(){
     const thisApp = this;
@@ -56,6 +108,8 @@ const app = {
 
   init: function(){
     const thisApp = this;
+
+    thisApp.initPages();
 
     thisApp.initData();
     thisApp.initCart();
