ad2bd7ff27b4df48c9fcdf2880496e4629c6b236
Marcin Lech <arcinech@gmail.com>
2022-03-14 20:33:27 +0100

    Add: basic of initWidget for booking

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,26 +1,34 @@
-import { templates } from '../settings.js';
+import { templates, select } from '../settings.js';
+import AmountWidget from './AmountWidget.js';
 
 class Booking{
   constructor(element){
     this.getElements(element);
-    this.renderBooking(element);
+    this.render(element);
     this.initWidgets();
   }
-
   getElements(element){
     const thisBooking = this;
     thisBooking.dom  = {};
     thisBooking.dom.wrapper = element;
+    thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
   }
-
-  renderBooking(element){
+  render(){
     /* generate HTML based on template */
     const generatedHTML = templates.bookingWidget();
-
-    element.innerHTML = generatedHTML;
+    this.dom.wrapper.innerHTML = generatedHTML;
   }
 
   initWidgets(){
+    this.peopleAmount = new AmountWidget(this.dom.peopleAmount);
+    this.dom.peopleAmount.addEventListener('change', function(){
+
+    });
+    this.dom.hoursAmount = new AmountWidget(this.dom.hoursAmount);
+    this.dom.hoursAmount.addEventListener('change', function(){
+
+    });
 
   }
 }
