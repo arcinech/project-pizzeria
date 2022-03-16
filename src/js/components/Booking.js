1c0a6b6916641cfe4b57db4a75047a72e24b6065
Marcin Lech <arcinech@gmail.com>
2022-03-16 18:09:18 +0100

    Add: getData start of work

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,5 @@
-import { templates, select } from '../settings.js';
+import { templates, select, settings } from '../settings.js';
+import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
@@ -7,7 +8,53 @@ class Booking{
   constructor(element){
     this.render(element);
     this.initWidgets();
+    this.getData();
   }
+
+  getData(){
+    const thisBooking = this;
+
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+    const endDateParam = settings.db.dateEndParamKey   + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
+    const params = {
+      booking: [
+        startDateParam,
+        endDateParam,
+      ],
+      eventsCurrent: [
+        settings.db.notRepeatParam,
+        startDateParam,
+        endDateParam,      
+      ],
+      eventsRepeat: [
+        settings.db.repeatParam,
+        endDateParam,     
+      ],
+    };
+
+    // console.log('getData params', params);
+
+    const urls = {
+      booking:      settings.db.url + '/' + settings.db.booking 
+                                    + '?' + params.booking.join('&'),
+      eventRepaet:  settings.db.url + '/' + settings.db.event   
+                                    + '?' + params.eventsCurrent.join('&'),
+      eventCurrent: settings.db.url + '/' + settings.db.event   
+                                    + '?' + params.eventsRepeat.join('&'),
+    };
+
+    // console.log('getData urls', urls);
+
+    fetch(urls.booking)
+      .then(function(bookingsResponse){
+        return bookingsResponse.json();
+      })
+      .then(function(bookings){
+        console.log(bookings);
+      });
+  }
+
 
   render(element){
     const thisBooking = this;
